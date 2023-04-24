9edf8a1ce807fb65ab7d33f921bd50fb3e806d95
hieupham20701 <19519511.hieu@student.edu.vn>
2023-04-24 22:08:40 +0700

    fix drug save request

--- src/main/java/com/medical/app/dto/request/DrugRequest.java
+++ src/main/java/com/medical/app/dto/request/DrugRequest.java
@@ -1,9 +1,13 @@
 package com.medical.app.dto.request;
 
+import com.medical.app.model.entity.CategoryDrug;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,6 +23,9 @@ public class DrugRequest {
     private Integer categoryDrugId;
     private String note;
     private String image;
+    private Date createdDate;
+    private Date updatedDate;
+    private Integer unitId;
     private Integer vienVi;
     private Integer viHop;
     private Integer hopThung;
